93ad34ad2277d65df693b9740c81c65cee7db69d
BrunoJPP <brunojpoker@gmail.com>
2023-10-05 00:54:23 -0300

    Criando Inimigo Atirador

--- Primeiro_Projeto/Assets/Scripts/GerenciadorInimigos.cs
+++ Primeiro_Projeto/Assets/Scripts/GerenciadorInimigos.cs
@@ -7,8 +7,13 @@ public class GerenciadorInimigos : MonoBehaviour
     [SerializeField]
     private GameObject prefabInimigo;
     [SerializeField]
+    private GameObject prefabInimigoAtirador;
+    [SerializeField]
     private float maxTime = 1;
     private float timeIcre = 0;
+
+    //private float x = 2;
+    //private float y = 8;
 
     void Update()
     {
@@ -18,6 +23,17 @@ public class GerenciadorInimigos : MonoBehaviour
             var x = Random.Range(-10.8f, 10.9f);
             Instantiate(prefabInimigo, new Vector2(x, 6.3f), transform.rotation);
             timeIcre= 0;
+
+            var b = Random.Range(-10.8f, 10.9f);
+            Instantiate(prefabInimigoAtirador, new Vector2(x, 6.3f), transform.rotation);
+            timeIcre = 0;
         }
     }
+
+    void NovosInimigos()
+    {
+        Instantiate(prefabInimigo, new Vector3(Random.Range(-2, 4), 5, 0), transform.rotation);
+        Instantiate(prefabInimigoAtirador, new Vector3(Random.Range(-4, 2), 8, 0), transform.rotation);
+    }
+
 }
--- Primeiro_Projeto/Assets/Scripts/InimigoController.cs
+++ Primeiro_Projeto/Assets/Scripts/InimigoController.cs
@@ -17,6 +17,14 @@ public class InimigoController : MonoBehaviour
     private void FixedUpdate()
     {
         _rb2d.MovePosition(_rb2d.position + new Vector2(0, _speed) * Time.deltaTime);
+
+        // Verifica se o inimigo passou da metade da tela no eixo Y.
+        if (transform.position.y < 0)
+        {
+            // Se passou da metade da tela, muda aleatoriamente a direção no eixo X.
+            float randomXDirection = Random.Range(-1f, 1f);
+            _rb2d.velocity = new Vector2(randomXDirection, _speed);
+        }
     }
 
     public void Destruir()
